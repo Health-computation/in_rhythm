ed1b634e10b35f445c57844b2e61fa0019ffafd2
Siddharth <Siddharth@10.33.131.108>
2013-11-18 19:19:59 -0500

    Deviation page added to charts. SleepBarChart and SleepOverlapChart now dynamically update from parse data. MainActivity and GoalSurveyActivity are now scrollable.

--- src/com/example/ds/ChartActivity.java
+++ src/com/example/ds/ChartActivity.java
@@ -1,13 +1,21 @@
 package com.example.ds;
 
+import java.util.List;
+
+
+import com.parse.*;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Typeface;
 import android.util.Log;
 import android.view.Menu;
+import android.view.TextureView;
 import android.view.View;
 import android.view.Window;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 public class ChartActivity extends Activity {
@@ -21,10 +29,60 @@ public class ChartActivity extends Activity {
 		setContentView(R.layout.activity_chart);
 		Typeface face = Typeface.createFromAsset(getAssets(),
 	            "proximanova-bold-webfont.ttf");
+		String defaultTab = null;
+		defaultTab = getIntent().getStringExtra("activePage"); //check which tab is the default launching tab
+		if(defaultTab == null){
+			defaultTab = "LOL";
+		}
+		
 		TextView competeText = (TextView) findViewById(R.id.sleepGraphText);
 		TextView barGraphText = (TextView) findViewById(R.id.barGraphText);
+		TextView deviationText = (TextView) findViewById(R.id.deviationText);			
+
+		
 		barGraphText.setTypeface(face);
 		competeText.setTypeface(face);
+		deviationText.setTypeface(face);
+
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("SleepDuration");
+		query.whereEqualTo("userID", 1);
+		query.setCachePolicy(ParseQuery.CachePolicy.NETWORK_ELSE_CACHE);
+		query.setLimit(7);
+		query.findInBackground(new FindCallback<ParseObject>() {
+		    public void done(List<ParseObject> sleepDurationList, ParseException e) {
+		        if (e == null) {
+		        	
+		        	//Redraw SleepBarChart on success
+		    		SleepBarChart sleepBarChart = (SleepBarChart) findViewById(R.id.sleepBarChart);
+		    		int[] newInformation = {0, 0, 0, 0, 0, 0, 0};
+		    		for(int i = 0; i < sleepDurationList.size(); ++i){
+		    			ParseObject current = sleepDurationList.get(i);
+		    			Integer sleepDuration = current.getInt("sleepDuration");
+		    			newInformation[i] = sleepDuration;
+		    		}
+		    		sleepBarChart.setInformation(newInformation);
+		    		sleepBarChart.invalidate();
+		    		
+		    		//Redraw SleepOverlapChart on success		    		
+		    		SleepOverlapChart sleepOverlapChart = (SleepOverlapChart) findViewById(R.id.sleepOverlapChart);
+		    		sleepOverlapChart.setInformation(sleepDurationList);
+		    		sleepOverlapChart.invalidate();
+		    		
+		        } else {
+		            Log.d("score", "Error: " + e.getMessage());
+		        }
+		    }
+		});
+
+		Log.d("LOL",defaultTab);
+		if(defaultTab.equals("2"))
+		{
+			Log.d("LOL", "IT's INSIDE");
+			View view = findViewById(R.id.sleepBarChart);
+	    	view.setVisibility(View.INVISIBLE);
+	    	view = findViewById(R.id.sleepOverlapChart);
+	    	view.setVisibility(View.VISIBLE);
+		}
 		
 		final TextView tv = (TextView) findViewById(R.id.sleepGraphText);
         tv.setOnClickListener(new View.OnClickListener() {
@@ -32,12 +90,13 @@ public class ChartActivity extends Activity {
 			@Override
 			public void onClick(View tv) {
 				// TODO Auto-generated method stub
-				
 			    	View view =  findViewById(R.id.sleepBarChart);
 			    	view.setVisibility(View.VISIBLE);
 			    	view = findViewById(R.id.sleepOverlapChart);
 			    	view.setVisibility(View.INVISIBLE);
 			    	
+
+				
 			    
 			}
         	
@@ -54,12 +113,20 @@ public class ChartActivity extends Activity {
 		    	view.setVisibility(View.INVISIBLE);
 		    	view = findViewById(R.id.sleepOverlapChart);
 		    	view.setVisibility(View.VISIBLE);
-			    	
-			    
+			    			    
 			}
         	
         });
 
+        final TextView tv2 = (TextView) findViewById(R.id.deviationText);
+        tv2.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v){
+		    	launchDeviationPage();
+		    }
+		});
+        
+        
 	}
 
 	@Override
@@ -68,6 +135,10 @@ public class ChartActivity extends Activity {
 		getMenuInflater().inflate(R.menu.chart, menu);
 		return true;
 	}
-	
+	private void launchDeviationPage(){
+		Intent DeviationPage = new Intent(this, DeviationActivity.class);
+		startActivity(DeviationPage);	
+		finish();
+	}
    
 }

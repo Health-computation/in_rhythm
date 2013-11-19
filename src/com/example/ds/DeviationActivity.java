a9d590826ebccd9f9dae9b88cd45e518ac0dc30b
Siddharth <Siddharth@10.33.131.108>
2013-11-18 22:38:50 -0500

    Deviation reasons aggregating properly. Micah++.

--- src/com/example/ds/DeviationActivity.java
+++ src/com/example/ds/DeviationActivity.java
@@ -1,5 +1,15 @@
 package com.example.ds;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -28,6 +38,60 @@ public class DeviationActivity extends Activity {
 		TextView Sleep =(TextView) findViewById(R.id.barGraphText);
 
 		Rhythm.setTypeface(face);
+		
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("Deviation");
+		//query.whereEqualTo("user_id", 3);
+		query.findInBackground(new FindCallback<ParseObject>() {
+		public void done(List<ParseObject> reasonsList, ParseException e) {
+		    if (e == null) {
+		    	Hashtable<Integer, Integer> reasonAggregation = new Hashtable<Integer, Integer>();
+
+		    	//List of all textview IDs to update
+		    	int[] textViews = {R.id.AcademicsValueTextView, R.id.SocialValueTextView, 
+		    			R.id.InsomniaValueTextView, R.id.CaffeineValueTextView, R.id.PhysicalValueTextView };
+		    	for(int i = 0; i < reasonsList.size(); ++i){
+			    	byte[] current = reasonsList.get(i).getBytes("reasons");
+			    	Log.d("CURRENT ", Integer.toString(current.length));
+		    		for(int reason = 1; reason < current.length; ++reason){
+		    			//Make reason value equal value of reason selection
+				    	//Log.d("HERE IS THE BYTE: ", Integer.toString(Integer.parseInt(Byte.toString(current[reason]))));
+		    			int reasonValue = Integer.parseInt(Byte.toString(current[reason]));
+				    	//Log.d("HERE IS THE BYTE: ", Integer.toString(reasonValue));
+		    			if(!reasonAggregation.containsKey(reason)){
+		    				reasonAggregation.put(reason, 1);
+		    			}else{
+		    				int currentCount = reasonAggregation.get(reason) + reasonValue;
+			    			reasonAggregation.put(reason, currentCount);	
+		    			}
+
+				    //	Log.d("HERE IS THE BYTE: ", Integer.toString(reasonValue));
+		    		}		    		
+		    	}
+		    	Log.d("LOL", "COOL AGGREGATION DONE");
+		    	for(int textView = 0; textView < textViews.length; ++textView){
+		    		int reasonCount = 0;
+	    			if(reasonAggregation.containsKey(textView + 1)){
+	    				reasonCount = reasonAggregation.get(textView + 1);
+	    			}	
+    				TextView currentTextView = (TextView) findViewById(textViews[textView]);
+    				currentTextView.setText(Integer.toString(reasonCount));
+		    	}
+		    	
+		    	
+		    	
+
+		    	//byte[] b = hexStringToByteArray(current);
+
+		    } else {
+		      // something went wrong
+		    	Log.d("IT DOESNT WORK",":(");
+		    }
+		  }
+		
+
+		
+		});
+		
 	
 		Rhythm.setOnClickListener(new View.OnClickListener() {
 			
@@ -61,5 +125,15 @@ public class DeviationActivity extends Activity {
 		startActivity(chartPage);
 		finish();
 	}	
+	
+	public static byte[] hexStringToByteArray(String s) {
+	    int len = s.length();
+	    byte[] data = new byte[len / 2];
+	    for (int i = 0; i < len; i += 2) {
+	        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+	                             + Character.digit(s.charAt(i+1), 16));
+	    }
+	    return data;
+	}
 		
 	}
